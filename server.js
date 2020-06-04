ef1005e54a289f6e0c2edec42df30a5e11130755
Ubuntu <ubuntu@ip-172-31-41-145.us-east-2.compute.internal>
2020-06-04 02:07:34 +0000

    added carousel module

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = 5500;
 
 //const HOST = process.env.HOST;
 
+
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/:id', express.static(path.join(__dirname, 'public')));
@@ -20,8 +21,8 @@ app.use('/:id', express.static(path.join(__dirname, 'public')));
 //app.use('/product/shop/:shopId', createProxyMiddleware({target: 'http://3.21.234.192:4200', changeOrigin: true}))
 //app.use('/product/colors/:id', createProxyMiddleware({target: 'http://3.21.234.192:4200', changeOrigin: true}))
 //app.use('/reviews/:id', createProxyMiddleware({target: 'http://fec-load-balancer-1328451394.us-east-2.elb.amazonaws.com', changeOrigin: true}))
-app.use('/api/carousel/:id', createProxyMiddleware({target: 'http://localhost:9000', changeOrigin: true}))
-app.use('/api/carouselEnlarged/:id', createProxyMiddleware({target: 'http://localhost:9000', changeOrigin: true}))
+app.use('/api/carousel/:id', createProxyMiddleware({target: 'http://18.220.5.4:9000', changeOrigin: true}))
+app.use('/api/carouselEnlarged/:id', createProxyMiddleware({target: 'http://18.220.5.4:9000', changeOrigin: true}))
 //app.use('/products/:id', createProxyMiddleware({target: 'http://ec2-3-22-170-203.us-east-2.compute.amazonaws.com:4000', changeOrigin: true}))
 //app.use('/get/random', createProxyMiddleware({target: 'http://localhost:9000', changeOrigin: true}))
 
